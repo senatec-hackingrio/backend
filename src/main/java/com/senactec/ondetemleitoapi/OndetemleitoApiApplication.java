40d09fa6aeb07dc0233c69e9bf87ebffc694ae1c
Hud Bolado <hudson.desenv@gmail.com>
2020-05-23 22:39:53 -0300

    Criando projeto

---
+++ src/main/java/com/senactec/ondetemleitoapi/OndetemleitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.senactec.ondetemleitoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndetemleitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndetemleitoApiApplication.class, args);
+	}
+
+}
